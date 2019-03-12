87052a6017b247ee8a22c5400a168ca51176dc9e
UlyanaKapeshina <UlyanaKapeshina@users.noreply.github.com>
2019-03-13 01:41:30 +0300

    исправлены замечания предзащиты

--- js/js-sedona.js
+++ js/js-sedona.js
@@ -1,8 +1,8 @@
 var link = document.querySelector(".open-form");
 var popup = document.querySelector(".modal-appointment-form");
-var close = popup.querySelector(".modal-close");
+// var close = popup.querySelector(".modal-close");
 var datearrival = popup.querySelector("[name=date-arrival]");
-var form = popup.querySelector("form");
+
 var datedeparture = popup.querySelector("[name=date-departure]");
 var adults = popup.querySelector("[name=adults]");
 var children = popup.querySelector("[name=children]");
@@ -18,8 +18,11 @@ try {
   isStorageSupport = false;
 }
 
+popup.classList.add("modal-hidden");
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
+  
   popup.classList.toggle("modal-show");
   popup.classList.remove("modal-error");
   datearrival.focus();
@@ -34,12 +37,12 @@ link.addEventListener("click", function (evt) {
    }    
  });
 
-form.addEventListener("submit", function (evt) {
+popup.addEventListener("submit", function (evt) {
   if (!datearrival.value || !datedeparture.value || !adults.value || !children.value ) {
   evt.preventDefault();
   popup.classList.remove("modal-error");
   popup.offsetWidth = popup.offsetWidth;
-  popup.classList.add("modal-error");
+     popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
       localStorage.setItem("adults", adults.value); 
