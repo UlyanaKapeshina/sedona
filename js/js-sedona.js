316cd55335b6be3d5316e46ed52c1779b823e721
UlyanaKapeshina <UlyanaKapeshina@users.noreply.github.com>
2019-03-13 14:40:15 +0300

    исправления по защите(попытка1)

--- js/js-sedona.js
+++ js/js-sedona.js
@@ -1,8 +1,6 @@
 var link = document.querySelector(".open-form");
 var popup = document.querySelector(".modal-appointment-form");
-// var close = popup.querySelector(".modal-close");
 var datearrival = popup.querySelector("[name=date-arrival]");
-
 var datedeparture = popup.querySelector("[name=date-departure]");
 var adults = popup.querySelector("[name=adults]");
 var children = popup.querySelector("[name=children]");
@@ -13,7 +11,7 @@ var storageChildren = "";
 
 try {
   storageAdults = localStorage.getItem("adults");
-  storageChildren = localStorage.getItem("children");        
+  storageChildren = localStorage.getItem("children");
 } catch (err) {
   isStorageSupport = false;
 }
@@ -22,33 +20,33 @@ popup.classList.add("modal-hidden");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
-  
+
   popup.classList.toggle("modal-show");
   popup.classList.remove("modal-error");
   datearrival.focus();
 
   if (storageAdults) {
     adults.value = storageAdults;
-    }    
- 
+  }
 
- if (storageChildren) {
+
+  if (storageChildren) {
     children.value = storageChildren;
-   }    
- });
+  }
+});
 
 popup.addEventListener("submit", function (evt) {
-  if (!datearrival.value || !datedeparture.value || !adults.value || !children.value ) {
-  evt.preventDefault();
-  popup.classList.remove("modal-error");
-  popup.offsetWidth = popup.offsetWidth;
-     popup.classList.add("modal-error");
+  if (!datearrival.value || !datedeparture.value || !adults.value || !children.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("adults", adults.value); 
-      localStorage.setItem("children", children.value);      
+      localStorage.setItem("adults", adults.value);
+      localStorage.setItem("children", children.value);
     }
-  }    
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
